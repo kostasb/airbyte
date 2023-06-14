c868981de09a253998cb77ab50ca608cd49301d4
alafanechere <augustin.lafanechere@gmail.com>
2023-06-14 20:51:16 +0200

    ref: more shared code between python and java connectors

--- tools/ci_connector_ops/ci_connector_ops/pipelines/format/__init__.py
+++ tools/ci_connector_ops/ci_connector_ops/pipelines/format/__init__.py
@@ -5,29 +5,60 @@
 
 from __future__ import annotations
 
+from typing import List
+
 import anyio
-from ci_connector_ops.pipelines.bases import ConnectorReport, StepResult
+from ci_connector_ops.pipelines.bases import ConnectorReport, Step, StepResult, StepStatus
 from ci_connector_ops.pipelines.contexts import ConnectorContext
 from ci_connector_ops.pipelines.format import java_connectors, python_connectors
+from ci_connector_ops.pipelines.git import GitPushChanges
 from ci_connector_ops.utils import ConnectorLanguage
+from dagger import Directory
 
 
 class NoFormatStepForLanguageError(Exception):
     pass
 
 
-LANGUAGE_FORMAT_CONNECTOR_MAPPING = {
-    ConnectorLanguage.PYTHON: python_connectors.run_connector_format,
-    ConnectorLanguage.LOW_CODE: python_connectors.run_connector_format,
-    ConnectorLanguage.JAVA: java_connectors.run_connector_format,
+FORMATTING_STEP_TO_CONNECTOR_LANGUAGE_MAPPING = {
+    ConnectorLanguage.PYTHON: python_connectors.FormatConnectorCode,
+    ConnectorLanguage.LOW_CODE: python_connectors.FormatConnectorCode,
+    ConnectorLanguage.JAVA: java_connectors.FormatConnectorCode,
 }
 
 
-async def run_connector_format(context: ConnectorContext) -> StepResult:
-    """Run a format pipeline for a single connector."""
-    if context.connector.language not in LANGUAGE_FORMAT_CONNECTOR_MAPPING:
-        raise NoFormatStepForLanguageError(f"No format step for connector language {context.connector.language}.")
-    return await LANGUAGE_FORMAT_CONNECTOR_MAPPING[context.connector.language](context)
+class ExportChanges(Step):
+
+    title = "Export changes to local repository"
+
+    async def _run(self, changed_directory: Directory, changed_directory_path_in_repo: str) -> StepResult:
+        await changed_directory.export(changed_directory_path_in_repo)
+        return StepResult(self, StepStatus.SUCCESS, stdout=f"Changes exported to {changed_directory_path_in_repo}")
+
+
+async def run_connector_format(context: ConnectorContext) -> List[StepResult]:
+    steps_results = []
+    FormatConnectorCode = FORMATTING_STEP_TO_CONNECTOR_LANGUAGE_MAPPING.get(context.connector.language)
+    if not FormatConnectorCode:
+        raise NoFormatStepForLanguageError(
+            f"No formatting step found for connector {context.connector.technical_name} with language {context.connector.language}"
+        )
+    format_connector_code_result = await FormatConnectorCode(context).run()
+    steps_results.append(format_connector_code_result)
+
+    if context.is_local:
+        export_changes_results = await ExportChanges(context).run(
+            format_connector_code_result.output_artifact, str(context.connector.code_directory)
+        )
+        steps_results.append(export_changes_results)
+    else:
+        git_push_changes_results = await GitPushChanges(context).run(
+            format_connector_code_result.output_artifact,
+            str(context.connector.code_directory),
+            f"Auto format {context.connector.technical_name} code",
+        )
+        steps_results.append(git_push_changes_results)
+    return steps_results
 
 
 async def run_connector_format_pipeline(context: ConnectorContext, semaphore: anyio.Semaphore) -> ConnectorReport:
--- tools/ci_connector_ops/ci_connector_ops/pipelines/format/java_connectors.py
+++ tools/ci_connector_ops/ci_connector_ops/pipelines/format/java_connectors.py
@@ -2,14 +2,10 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-from typing import List, Tuple
 
 import asyncer
 from ci_connector_ops.pipelines.actions import environments
 from ci_connector_ops.pipelines.bases import StepResult, StepStatus
-from ci_connector_ops.pipelines.contexts import ConnectorContext
-from ci_connector_ops.pipelines.format.common import ExportChanges
-from ci_connector_ops.pipelines.git import GitPushChanges
 from ci_connector_ops.pipelines.gradle import GradleTask
 from ci_connector_ops.pipelines.utils import with_exit_code, with_stderr, with_stdout
 
@@ -21,14 +17,11 @@ class FormatConnectorCode(GradleTask):
 
     title = "Format connector code"
 
-    def _get_gradle_command(self, extra_options: Tuple[str] = ...) -> List:
-        return ["./gradlew", "format"]
-
     async def _run(self) -> StepResult:
         formatted = (
             environments.with_gradle(self.context, self.build_include, bind_to_docker_host=self.BIND_TO_DOCKER_HOST)
             .with_mounted_directory(str(self.context.connector.code_directory), await self._get_patched_connector_dir())
-            .with_exec(self._get_gradle_command())
+            .with_exec(["./gradlew", "format"])
         )
         async with asyncer.create_task_group() as task_group:
             soon_exit_code = task_group.soonify(with_exit_code)(formatted)
@@ -42,23 +35,3 @@ class FormatConnectorCode(GradleTask):
             stdout=soon_stdout.value,
             output_artifact=formatted.directory(str(self.context.connector.code_directory)),
         )
-
-
-async def run_connector_format(context: ConnectorContext) -> List[StepResult]:
-    steps_results = []
-    format_connector_code_result = await FormatConnectorCode(context).run()
-    steps_results.append(format_connector_code_result)
-
-    if context.is_local:
-        export_changes_results = await ExportChanges(context).run(
-            format_connector_code_result.output_artifact, str(context.connector.code_directory)
-        )
-        steps_results.append(export_changes_results)
-    else:
-        git_push_changes_results = await GitPushChanges(context).run(
-            format_connector_code_result.output_artifact,
-            str(context.connector.code_directory),
-            f"Auto format {context.connector.technical_name} code",
-        )
-        steps_results.append(git_push_changes_results)
-    return steps_results
